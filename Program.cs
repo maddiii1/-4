83a820b444117733d446081bed794655a3a550c7
Madina Mammadova <mamedovamadinaa8@gmail.com>
2022-12-06 22:28:19 +0300

    Task 27 done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и метод который возводит число A в натуральную степень B. Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль. Не использовать Math.Pow()
-
+/*
 int degree(int number,int stepen){
 int rez=1;
 
@@ -20,11 +20,24 @@ B = Convert.ToInt32(Console.ReadLine());
 
 int v = degree(A,B);
 Console.Write("Результат: "+ v); 
+*/
+
 
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 //452 -> 11
 //82 -> 10
 //9012 -> 12
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
+int sum = 0; 
+for (;number > 0;)
+{
+   int lastNumber = number%10;
+   sum = sum + lastNumber;
+   number = number/10;
+}
+
+Console.Write("Результат суммы: " + sum);
 
